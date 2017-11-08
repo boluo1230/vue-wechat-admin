92b538b06ad66052032d7d22be64d8fdfa6bed77
keikeizhang <698088330@qq.com>
2017-11-08 09:26:14 +0800

    更新title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="X-CSRF-TOKEN" content="{{csrf_token()}}">
-    <title>数字站台内容管理系统</title>
+    <title>VueAdmin</title>
 </head>
 <body>
 <div id="app">
