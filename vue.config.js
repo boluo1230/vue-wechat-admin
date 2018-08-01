40056b41e67981999b69d7e7e84e1745a4f79de0
JsonZhang <json@bubblelab.com>
2018-08-01 12:48:30 +0800

    生成webpack配置文件 但是有错误

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,12 @@ const resolve = dir => {
   return path.join(__dirname, dir)
 }
 
-// module.exports = {
-//   chainWebpack: config => {
-//     config.resolve.alias
-//       .set('@', resolve('src'))
-//       .set('_c', resolve('src/components'))
-//       .set('_conf', resolve('config'))
-//   },
-//   // 打包时不生成.map文件
-//   productionSourceMap: false
-// }
+module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('_c', resolve('src/components'))
+  },
+  // 打包时不生成.map文件
+  productionSourceMap: false
+}
