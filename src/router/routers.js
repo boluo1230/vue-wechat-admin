ec5e0ea19a6a6708e04c49f33676e9a879e96fbe
JsonZhang <json@bubblelab.com>
2018-08-01 12:34:35 +0800

    基础模版

---
+++ src/router/routers.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    path: '/home',
+    name: 'home',
+    meta: {
+      title: 'Home',
+      hideInMenu: true
+    },
+    component: () => import('../view/home')
+  }
+]
---
+++ src/view/home/home.vue
@@ -0,0 +1,12 @@
+<template>
+  <span class="home-css">
+    This is home page
+  </span>
+</template>
+
+<style scoped>
+.home-css {
+  font-size: 32px;
+}
+</style>
+
