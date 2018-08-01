643835710bcf5f28b545fc24a3fe1efb925f1d50
JsonZhang <json@bubblelab.com>
2018-08-01 12:47:48 +0800

    生成webpack配置文件 但是有凑五

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+
+// module.exports = {
+//   chainWebpack: config => {
+//     config.resolve.alias
+//       .set('@', resolve('src'))
+//       .set('_c', resolve('src/components'))
+//       .set('_conf', resolve('config'))
+//   },
+//   // 打包时不生成.map文件
+//   productionSourceMap: false
+// }
